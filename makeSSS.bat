db45cb3871bc357b2920199220050a0975c7e1e7
Akif Eyler <ma.eyler@gmail.com>
2015-09-21 09:50:31 +0300

    V2.01

--- makeSSS.bat
+++ makeSSS.bat
@@ -1,5 +1,6 @@
 del sss.jar
 \java\jdk7\bin\jar cfm sss.jar mae\sss\sss.MF
-\java\jdk7\bin\jar uf  sss.jar mae\sss\*.*   mae\tut\num\*.class
-\java\jdk7\bin\jar uf  sss.jar mae\brow\*.*  mae\brow\img\*.*
-\java\jdk7\bin\jar uf  sss.jar mae\util\*.*  mae\util\*.properties  mae\util\*.gif
+\java\jdk7\bin\jar uf  sss.jar mae\sss\*.*
+\java\jdk7\bin\jar uf  sss.jar mae\tut\num\*.*
+\java\jdk7\bin\jar uf  sss.jar mae\brow\*.*
+\java\jdk7\bin\jar uf  sss.jar mae\util\*.*
